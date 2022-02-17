557123d1fbe668d1939dae8ea5848912a1a9e247
Ranidhumma <ranidhumma8@gmail.com>
2022-02-17 15:06:08 +0530

    Repeat_Position reaches_100

--- src/com/java/Snakeladder.java
+++ src/com/java/Snakeladder.java
@@ -3,18 +3,37 @@ package com.java;
 import java.util.Random;
 
 public class Snakeladder {
-
+	
 	static int position = 0; 
+	static final int NO_PLAY = 0;
+	static final int LADDER = 1;
+	static final int SNAKE = 2;
 	
 	public static int diceroll() {
 	    Random random = new Random();
 		int dice = random.nextInt(6)+1;
 		return dice;
 	}
-	
-
-	public static void main(String[] args) {
-	System.out.println("dice= " +diceroll());
+	public static void checkOption(int dice) {
+		Random random = new Random();
+		int option = random.nextInt(3);
+		if (option == LADDER) {
+			  position += dice;
+		}
+		else if (option == SNAKE) {
+			   position -= dice;
+		}
+		else {
+			   position = position;
+		}
+	}
+			public static void main(String[] args) {
+				while(position<100) {
+					int dice = diceroll();
+	System.out.println("dice= " +dice);
+	 checkOption(dice);
+	System.out.println("Position= " +position);
 	}
 
+}
 }
