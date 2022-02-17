4362e6228a932aa2ce8761f3cf2f4e5e65643e6f
Ranidhumma <ranidhumma8@gmail.com>
2022-02-17 14:04:21 +0530

    diceRoll

--- src/com/java/Snakeladder.java
+++ src/com/java/Snakeladder.java
@@ -1,10 +1,20 @@
 package com.java;
 
+import java.util.Random;
+
 public class Snakeladder {
 
-	static int position = 0;
+	static int position = 0; 
+	
+	public static int diceroll() {
+	    Random random = new Random();
+		int dice = random.nextInt(6)+1;
+		return dice;
+	}
+	
 
 	public static void main(String[] args) {
+	System.out.println("dice= " +diceroll());
 	}
 
 }
